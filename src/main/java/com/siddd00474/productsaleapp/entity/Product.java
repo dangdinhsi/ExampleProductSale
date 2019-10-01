889b3e2d576630ec699e14dd0b528007edfd6a64
dangdinhsi <siddd00474@fpt.edu.vn>
2019-10-01 10:54:35 +0700

    sale product example

--- src/main/java/com/siddd00474/productsaleapp/entity/Product.java
+++ src/main/java/com/siddd00474/productsaleapp/entity/Product.java
@@ -14,4 +14,52 @@ public class Product {
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "slNo")
     private Sale sale;
+
+    public long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(long productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDesciption() {
+        return Desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        Desciption = desciption;
+    }
+
+    public long getDateOfManf() {
+        return DateOfManf;
+    }
+
+    public void setDateOfManf(long dateOfManf) {
+        DateOfManf = dateOfManf;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Sale getSale() {
+        return sale;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
 }
--- src/main/java/com/siddd00474/productsaleapp/entity/Sale.java
+++ src/main/java/com/siddd00474/productsaleapp/entity/Sale.java
@@ -1,8 +1,10 @@
 package com.siddd00474.productsaleapp.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class Sale {
@@ -11,8 +13,48 @@ public class Sale {
     private long SalesmanID;
     private String SalesmanName;
     private long dos;
+    @OneToMany(mappedBy = "Product", cascade = CascadeType.ALL)
+    private Set<Product> products;
 
+    public long getSlNo() {
+        return slNo;
+    }
 
+    public void setSlNo(long slNo) {
+        this.slNo = slNo;
+    }
+
+    public long getSalesmanID() {
+        return SalesmanID;
+    }
+
+    public void setSalesmanID(long salesmanID) {
+        SalesmanID = salesmanID;
+    }
+
+    public String getSalesmanName() {
+        return SalesmanName;
+    }
+
+    public void setSalesmanName(String salesmanName) {
+        SalesmanName = salesmanName;
+    }
+
+    public long getDos() {
+        return dos;
+    }
+
+    public void setDos(long dos) {
+        this.dos = dos;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
 
 
